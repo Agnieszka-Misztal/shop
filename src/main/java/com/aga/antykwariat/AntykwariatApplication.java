6029f19b3766a1d21f2d660635edfb6a69da03f4
Aga <mishyag@gmail.com>
2019-01-02 21:50:14 +0100

    baza pod springa/dependecies

---
+++ src/main/java/com/aga/antykwariat/AntykwariatApplication.java
@@ -0,0 +1,14 @@
+package com.aga.antykwariat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntykwariatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntykwariatApplication.class, args);
+	}
+
+}
+
